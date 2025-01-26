64d3a36afcc026fa1402fcc9bd5c5be894d0ce10
axelquartz <axelarep@gmail.com>
2025-01-26 15:51:40 -0600

    Add working input of main products. Push them in an array

--- src/script.js
+++ src/script.js
@@ -27,3 +27,63 @@ const boneless = new Item ({
 })
 
 bonelessAddBtn.addEventListener('click', boneless.calcPrice)
+
+// Alitas
+const alitasAddBtn = document.getElementById('alitas-add-btn')
+const userAlitasQuantity = document.getElementById('user-alitas-quantity')
+
+const alitas = new Item ({
+    name: 'Alitas',
+    unitPrice: 75,
+    userQuantity: userAlitasQuantity,
+})
+
+alitasAddBtn.addEventListener('click', alitas.calcPrice)
+
+// Dedos de Queso
+const dedosAddBtn = document.getElementById('dedos-add-btn')
+const userDedosQuantity = document.getElementById('user-dedos-quantity')
+
+const dedos = new Item ({
+    name: 'Dedos de Queso',
+    unitPrice: 120,
+    userQuantity: userDedosQuantity,
+})
+
+dedosAddBtn.addEventListener('click', dedos.calcPrice)
+
+// Papas Francesas
+const francesasAddBtn = document.getElementById('francesas-add-btn')
+const userFrancesasQuantity = document.getElementById('user-francesas-quantity')
+
+const francesas = new Item ({
+    name: 'Papas Francesas',
+    unitPrice: 160,
+    userQuantity: userFrancesasQuantity,
+})
+
+francesasAddBtn.addEventListener('click', francesas.calcPrice)
+
+// Papas Gajo
+const gajoAddBtn = document.getElementById('gajo-add-btn')
+const userGajoQuantity = document.getElementById('user-gajo-quantity')
+
+const gajo = new Item ({
+    name: 'Papas Gajo',
+    unitPrice: 165,
+    userQuantity: userGajoQuantity,
+})
+
+gajoAddBtn.addEventListener('click', gajo.calcPrice)
+
+// Salchicha
+const salchichaAddBtn = document.getElementById('salchicha-add-btn')
+const userSalchichaQuantity = document.getElementById('user-salchicha-quantity')
+
+const salchicha = new Item ({
+    name: 'Salchicha',
+    unitPrice: 100,
+    userQuantity: userSalchichaQuantity,
+})
+
+salchichaAddBtn.addEventListener('click', salchicha.calcPrice)
